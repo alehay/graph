b31f02d638cefd067fd80931a03d093451b34e4a
kudakov <kudakov@inspector-cloud.com>
2024-12-24 00:38:41 +0300

    remove old code

--- src/render/graphRenderer.hpp
+++ src/render/graphRenderer.hpp
@@ -10,67 +10,6 @@
 #include <glm/gtc/type_ptr.hpp>
 //// clang-format on
 
-
-#if 0
-template <typename VertexProperty, typename EdgeProperty> class GraphRenderer {
-private:
-  Graph<VertexProperty, EdgeProperty> *graph;
-  float windowWidth;
-  float windowHeight;
-
-public:
-  GraphRenderer(Graph<VertexProperty, EdgeProperty> *g, float width,
-                float height)
-      : graph(g), windowWidth(width), windowHeight(height) {}
-
-  void render() {
-    glViewport(0, 0, windowWidth, windowHeight);
-    glMatrixMode(GL_PROJECTION);
-    glLoadIdentity();
-    glOrtho(0, windowWidth, windowHeight, 0, -1, 1);
-    glMatrixMode(GL_MODELVIEW);
-    glLoadIdentity();
-
-    glClearColor(1.0f, 1.0f, 1.0f, 1.0f);
-    glClear(GL_COLOR_BUFFER_BIT);
-
-    GLenum error = glGetError();
-    if (error != GL_NO_ERROR) {
-      printf("OpenGL error: %d\n", error);
-    }
-    // graph->calculateLayout();
-    drawVertices();
-    drawEdges();
-  }
-
-private:
-  void drawVertices() {
-    glColor3f(1.0f, 0.0f, 0.0f);
-    glPointSize(10.0f);
-
-    glBegin(GL_POINTS);
-    for (const auto &[x, y] : graph->getVertexPositions()) {
-      glVertex2f(x, y);
-    }
-    glEnd();
-  }
-
-  void drawEdges() {
-    glLineWidth(1);
-    glColor3f(0.4f, 0.4f, 1.0f);
-
-    glBegin(GL_LINES);
-    for (const auto &edge : graph->get2DEdgePositions()) {
-      auto &source = edge.first;
-      auto &target = edge.second;
-      glVertex2f(source.first, source.second);
-      glVertex2f(target.first, target.second);
-    }
-    glEnd();
-  }
-};
-#endif
-
 template <typename VertexProperty, typename EdgeProperty>
 class GraphRenderer {
 private:
